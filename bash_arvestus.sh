532d62a3b38f3c2b0a610eb1d351167052fc6ed9
Fantyy <priit.varul@gmail.com>
2014-05-16 14:24:00 +0300

    valmis
    valmis

---
+++ bash_arvestus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Autor - Priit Varul A21
+# Kuupäev - 16.05.2014
+# Skript leiab üles kõik käsurealt etteantud kasutajale kuuluvad failid, kogu failisüsteemist. Failide nimekiri kirjutatakse faili kasutajanimi.txt ja sorteeritakse suuruse järgi. Failis oleks näha nii suurus kui ka failinimi täis pika teena.
+
+if [ $# -ne 1 ]
+then 
+    echo "parameetrite arv on vale!"
+exit 1
+fi
+    
+find / -type f -user $1 -exec ls -l {} \; > /tmp/output.txt
+chmod 777 /tmp/output.txt
+sort -k 5 < /tmp/output.txt > output2.txt
+rm /tmp/output.txt
